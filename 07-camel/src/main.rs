272fa696b8e1cd6736c40b44de6738acd6b82428
lh317 <46079517+lh317@users.noreply.github.com>
2023-12-07 09:37:54 -0500

    Day 7 Part 1

---
+++ 07-camel/src/main.rs
@@ -0,0 +1,111 @@
+use std::{cmp::Reverse, collections::BTreeMap};
+use std::fs;
+use std::collections::HashMap;
+use std::str::FromStr;
+use eyre::{eyre, Report, Result};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum Card {
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace
+}
+
+impl FromStr for Card {
+    type Err = Report;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "2" => Ok(Card::Two),
+            "3" => Ok(Card::Three),
+            "4" => Ok(Card::Four),
+            "5" => Ok(Card::Five),
+            "6" => Ok(Card::Six),
+            "7" => Ok(Card::Seven),
+            "8" => Ok(Card::Eight),
+            "9" => Ok(Card::Nine),
+            "T" => Ok(Card::Ten),
+            "J" => Ok(Card::Jack),
+            "Q" => Ok(Card::Queen),
+            "K" => Ok(Card::King),
+            "A" => Ok(Card::Ace),
+            _ => Err(eyre!("unknown card '{}'", s)),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum HandKind {
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+struct Hand {
+    kind: HandKind,
+    cards: Vec<Card>
+}
+
+impl FromStr for Hand {
+    type Err = Report;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() != 5 {
+            return Err(eyre!("hand length {} must be 5", s.len()));
+        }
+        let cards = s.chars().map(|c| c.to_string().parse::<Card>()).collect::<Result<Vec<_>>>()?;
+        let mut counts = HashMap::new();
+        for card in cards.iter() {
+            *counts.entry(*card).or_insert(0) += 1;
+        }
+        let mut counts: Vec<_> = counts.into_values().collect();
+        counts.sort_by_key(|c| Reverse(*c));
+        let kind = match counts[0] {
+            5 => HandKind::FiveOfAKind,
+            4 => HandKind::FourOfAKind,
+            3 => match counts[1] {
+                2 => HandKind::FullHouse,
+                _ => HandKind::ThreeOfAKind,
+            },
+            2 => match counts[1] {
+                2 => HandKind::TwoPair,
+                _ => HandKind::OnePair
+            },
+            1 => HandKind::HighCard,
+            _ => panic!("logic error")
+        };
+        Ok(Hand { kind, cards} )
+    }
+}
+
+fn main() -> Result<()> {
+    let mut args = std::env::args();
+    let fname = args.nth(1).ok_or(eyre!("filename was not provided"))?;
+    let body = fs::read_to_string(fname.clone())?;
+    let mut lines = body.lines();
+    let mut rankings = BTreeMap::new();
+    for (lineno, line) in lines.enumerate() {
+        let (hand_str, ranking_str) = line.split_once(' ').ok_or(eyre!("{}:{}: no split", fname, lineno+1))?;
+        let hand: Hand = hand_str.parse()?;
+        let ranking: u32 = ranking_str.parse()?;
+        rankings.insert(hand, ranking);
+    }
+    let winnings: usize = rankings.into_iter().enumerate().map(|(i, (_, bet))| (i + 1) * bet as usize).sum();
+    println!("{}", winnings);
+    Ok(())
+}
