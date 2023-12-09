e305a4e8e8eb697819d8dc734a841a1d846698c7
lh317 <46079517+lh317@users.noreply.github.com>
2023-12-09 01:18:23 -0500

    Day 9 Part 2

--- 09-mirage/src/main.rs
+++ 09-mirage/src/main.rs
@@ -5,7 +5,7 @@ fn main() -> Result<()> {
     let mut args = std::env::args();
     let fname = args.nth(1).ok_or_else(|| eyre!("filename was not provided"))?;
     let body = fs::read_to_string(fname.clone())?;
-    let sum: i64 = body
+    let (first_sum, last_sum): (i64, i64) = body
         .lines()
         .enumerate()
         .map(|(lineno, l)| {
@@ -17,22 +17,28 @@ fn main() -> Result<()> {
                 })
                 .collect::<Result<_>>()?;
             if seq.len() >= 2 {
+                let mut first = Vec::new();
                 let mut last = Vec::new();
+                first.push(*seq.first().unwrap());
                 last.push(*seq.last().unwrap());
                 let mut diff: Vec<_> = seq.windows(2).map(|w| w[1] - w[0]).collect();
                 loop {
+                    first.push(*diff.first().unwrap());
                     last.push(*diff.last().unwrap());
                     if diff.iter().all(|&d| d == diff[0]) {
                         break;
                     }
                     diff = diff.windows(2).map(|w| w[1] - w[0]).collect();
                 }
-                Ok(last.into_iter().sum::<i64>())
+                // Note the order of ops in the rfold: subtract previous diff - accumulated total
+                // to go "up" the triangle.
+                Ok((first.into_iter().rfold(0, |acc, f| f - acc), last.into_iter().sum::<i64>()))
             } else {
-                Err(eyre!("{}:{}: too few numbers", fname, lineno +1))
+                Err(eyre!("{}:{}: too few numbers", fname, lineno + 1))
             }
         })
-        .sum::<Result<_>>()?;
-    println!("{}", sum);
+        .try_fold((0, 0), |(f0, l0), r| r.map(|(f, l)| (f0 + f, l0 + l)))?;
+    println!("{}", last_sum);
+    println!("{}", first_sum);
     Ok(())
 }
