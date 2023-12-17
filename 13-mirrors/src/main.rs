3c72234ef0b835496ae2dbe32eb6010827a86bbd
lh317 <46079517+lh317@users.noreply.github.com>
2023-12-17 08:34:59 -0500

    Day 13 Part 2

--- 13-mirrors/src/main.rs
+++ 13-mirrors/src/main.rs
@@ -1,5 +1,7 @@
-use eyre::{eyre, Report, Result, WrapErr};
-use ndarray::Array2;
+#![allow(clippy::iter_nth_zero)]
+
+use eyre::{eyre, Report, Result, WrapErr, OptionExt};
+use ndarray::{Array2, ArrayBase, Axis, Data, Ix2};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Terrain {
@@ -7,6 +9,14 @@ enum Terrain {
     Rocks,
 }
 
+impl Terrain {
+    fn flip(&mut self) {
+        *self = match self {
+            Terrain::Ash => Terrain::Rocks,
+            Terrain::Rocks => Terrain::Ash
+        };
+    }
+}
 impl TryFrom<char> for Terrain {
     type Error = Report;
 
@@ -44,6 +54,32 @@ fn parse_array<'a>(lines: impl Iterator<Item = &'a str>) -> Option<Result<Array2
     }
 }
 
+fn mirror<'a, T: PartialEq + 'a, D: Data<Elem = T>>(array: &'a ArrayBase<D, Ix2>) -> impl Iterator<Item=(Axis, usize)> + '_ {
+
+    let col_iter = array.columns().into_iter();
+    let col_iter_s1 = col_iter.clone().skip(1);
+    let col_combined = col_iter.zip(col_iter_s1).enumerate().filter_map(|(i, (c0, c1))| {
+        let (_, cols) = array.dim();
+        if c0 == c1 && (0..i).rev().zip((i+2)..cols).all(|(lc, rc)| array.column(lc) == array.column(rc)) {
+            Some((Axis(1), i))
+        } else {
+            None
+        }
+    });
+    let row_iter = array.rows().into_iter();
+    let row_iter_s1 = row_iter.clone().skip(1);
+    let row_combined = row_iter.zip(row_iter_s1).enumerate().filter_map(|(i, (r0, r1))| {
+        let (rows, _) = array.dim();
+        if r0 == r1 && (0..i).rev().zip((i + 2)..rows).all(|(lr, rr)| array.row(lr) == array.row(rr)) {
+            Some((Axis(0), i))
+        } else {
+            None
+        }
+    });
+    col_combined.chain(row_combined)
+}
+
+
 fn main() -> Result<()> {
     let mut args = std::env::args();
     let fname = args.nth(1).ok_or_else(|| eyre!("filename was not provided"))?;
@@ -54,43 +90,40 @@ fn main() -> Result<()> {
         arrays.push(array?);
     }
     let mut sum = 0usize;
-    for array in arrays.iter() {
-        let (rows, cols) = array.dim();
-        for col in array
-            .columns()
-            .into_iter()
-            .zip(array.columns().into_iter().skip(1))
-            .enumerate()
-            .filter_map(|(i, (col, next_col))| {
-                if col == next_col {
-                    Some(i)
-                } else {
-                    None
-                }
-            })
-        {
-            if (0..col).rev().zip(col + 2..cols).all(|(c, mc)| array.column(c) == array.column(mc))
-            {
-                sum += col + 1;
-            }
-        }
-
-        for row in
-            array.rows().into_iter().zip(array.rows().into_iter().skip(1)).enumerate().filter_map(
-                |(i, (row, next_row))| {
-                    if row == next_row {
-                        Some(i)
-                    } else {
-                        None
-                    }
-                },
-            )
-        {
-            if (0..row).rev().zip(row + 2..rows).all(|(r, mr)| array.row(r) == array.row(mr)) {
-                sum += 100 * (row + 1);
-            }
-        }
+    let mut results = Vec::new();
+    for array in arrays.iter_mut() {
+        let answer = mirror(array).nth(0).ok_or_eyre("did not find mirror")?;
+        results.push(answer);
+        sum += match answer {
+            (Axis(0), x) => 100 * (x + 1),
+            (Axis(1), y) => y + 1,
+            _ => unreachable!(),
+        };
     }
     println!("{sum}");
+    let mut sum2 = 0usize;
+    'array: for (array, answer) in arrays.iter_mut().zip(results.iter()) {
+        let (rows, cols) = array.dim();
+        for row in 0..rows {
+            for col in 0..cols {
+                array[[row, col]].flip();
+                if let Some(new) = mirror(array).filter(|a| a != answer).nth(0) {
+                    sum2 += match new {
+                        (Axis(0), x) => 100 * (x + 1),
+                        (Axis(1), y) => y+1,
+                        _ => unreachable!()
+                    };
+                    continue 'array;
+                }
+                array[[row, col]].flip();
+            }
+        }
+        // sum2 += match *answer {
+        //     (Axis(0), x) => 100 * (x + 1),
+        //     (Axis(1), y) => y+1,
+        //     _ => unreachable!()
+        // };
+    }
+    println!("{sum2}");
     Ok(())
 }
