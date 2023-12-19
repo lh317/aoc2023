fa661fb1bbf6220c0cab686a1ec352f300c691a8
lh317 <46079517+lh317@users.noreply.github.com>
2023-12-18 22:49:10 -0500

    Day 15 Part 2

--- 15-lens/src/main.rs
+++ 15-lens/src/main.rs
@@ -1,6 +1,8 @@
-use std::hash::{Hasher, Hash};
+use std::hash::Hasher;
+use std::str::FromStr;
 
-use eyre::{eyre, Result};
+use eyre::{eyre, Report, Result, OptionExt};
+use indexmap::IndexMap;
 
 #[derive(Debug, Clone, Copy, Default)]
 struct HolidayHasher {
@@ -27,6 +29,61 @@ impl Hasher for HolidayHasher {
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum InstructionKind {
+    Remove,
+    Add(u8)
+}
+
+impl FromStr for InstructionKind {
+    type Err = Report;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s == "-" {
+            Ok(InstructionKind::Remove)
+        } else if s.starts_with('=') {
+            Ok(InstructionKind::Add(s.strip_prefix('=').unwrap().parse()?))
+        } else {
+            Err(eyre!("unknown instruction '{s}'"))
+        }
+    }
+}
+
+
+#[derive(Debug, Clone, Copy)]
+struct Instruction<'source> {
+    label: &'source str,
+    op: InstructionKind,
+}
+
+impl<'a> Instruction<'a> {
+    fn label(&self) -> &'a str {
+        self.label
+    }
+
+    fn op(&self) -> &InstructionKind {
+        &self.op
+    }
+
+    fn light_box(&self) -> usize {
+        let mut hasher = HolidayHasher::new();
+        hasher.write(self.label.as_bytes());
+        hasher.finish() as usize
+    }
+}
+
+impl<'source> TryFrom<&'source str> for Instruction<'source> {
+    type Error = Report;
+
+    fn try_from(s: &'source str) -> Result<Self, Self::Error> {
+        let index = s.find(['=', '-']).ok_or_eyre("invalid instruction '{s}'")?;
+        let (label, op_str) = s.split_at(index);
+        let op = op_str.parse()?;
+        Ok(Self {label, op })
+    }
+}
+
+
 fn main() -> Result<()> {
     let mut args = std::env::args();
     let fname = args.nth(1).ok_or_else(|| eyre!("filename was not provided"))?;
@@ -34,10 +91,21 @@ fn main() -> Result<()> {
     let sum: u64 = body.trim_end().split(',').map(|s| {
         let mut hasher = HolidayHasher::new();
         hasher.write(s.as_bytes());
-        let result = hasher.finish();
-        //println!("{s}: {result}");
-        result
+        hasher.finish()
     }).sum();
     println!("{sum}");
+    let mut boxes: [IndexMap<&str, u8>; 256] = array_init::array_init(|_| IndexMap::new());
+    for s in body.trim_end().split(',') {
+        let inst: Instruction = s.try_into()?;
+        let b = &mut boxes[inst.light_box()];
+        match inst.op() {
+            InstructionKind::Remove => {b.shift_remove(inst.label());},
+            InstructionKind::Add(lens) => {*b.entry(inst.label()).or_default() = *lens;},
+        };
+    }
+    let sum2: usize = boxes.iter().enumerate().flat_map(|(i, b)| {
+        b.iter().enumerate().map(move |(j, (_, lens))| (i+1)*(j+1)*(*lens as usize))
+    }).sum();
+    println!("{sum2}");
     Ok(())
 }
